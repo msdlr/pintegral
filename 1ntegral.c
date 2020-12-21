687ff780d47de0a9114b589e7390fae4977eed67
Tu Nombre <you@example.com>
2020-12-21 18:09:13 +0100

    cálculo de un área

--- 1ntegral.c
+++ 1ntegral.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 
+#define f(x) (float)(4.0/(1.0 + x*x))
+
+/*
+    VARIABLES GLOBALES
+*/
+
+float anchura_intervalo;
+unsigned short n_rectangulos;
+unsigned short n_hilos;
+
+/*
+    FUNTIONES
+*/
+
+// Calcula f(x) en el punto medio de dos valores de X pasados por parámetro
+float calcula_area(float pto_medio){
+    return f(pto_medio)*(anchura_intervalo));
+}
 
 void main(int argc, char *argv[]){
     printf("Integrando...\n");
