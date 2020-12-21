04bc1de938ddab65f36cf004df671f900a69e5db
pgm-n117 <pgm.atctone@gmail.com>
2020-12-21 18:11:41 +0100

    Added function threads

--- 1ntegral.c
+++ 1ntegral.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 
 
+
+
+
+
+/*
+    nt : number of threads
+    nr : number of rectangles
+*/
+void threads(int nt, int nr){
+    
+}
+
+
 void main(int argc, char *argv[]){
     printf("Integrando...\n");
 }
